97e75db7d9059cf5f652ec46ff1cfc47db0fce58
kor <kor@dwarshuis.com>
2024-06-08 22:39:40 +0200

    Add anchors to terms (<dt>), just like the headings

---
+++ assets/js/addAnchorsToTerms.js
@@ -0,0 +1,19 @@
+function addAnchorsToTerms() {
+
+    const dts = document.querySelectorAll('dt > span');
+    console.log('dts: ', dts);
+
+    dts.forEach(dt => {
+        // dt.classList.add('toc-anchor');
+        const id = dt.getAttribute('id');
+        const a = document.createElement('a');
+        a.setAttribute('href', `#${id}`);
+        a.setAttribute('class', 'toc-anchor');
+        a.innerHTML = '§ ';
+        dt.parentNode.insertBefore(a, dt);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    addAnchorsToTerms();
+});
