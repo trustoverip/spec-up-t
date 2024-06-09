3b950d2e2fd3f43c3b031cca8712abf6d78f5369
kor <kor@dwarshuis.com>
2024-06-09 18:28:57 +0200

    New console.log for testing

--- src/get-xrefs-data.js
+++ src/get-xrefs-data.js
@@ -15,7 +15,7 @@ const projectRoot = process.cwd();
 
 // Create a path for the output file in the project root
 const outputPath = path.join(projectRoot, 'output/xrefs-data.js');
-console.log('outputPath: ', outputPath);
+console.log('projectRoot: ', projectRoot);
 
 function getXrefsData() {
     let allXrefs = {};
