9753eb28e035b5bd6f15d5453351750b3abfb6a2
kor <kor@dwarshuis.com>
2024-08-31 07:34:23 +0200

    Fix jump issue and make toggle button show expand / collapse symbol

--- assets/js/collapse-definitions.js
+++ assets/js/collapse-definitions.js
@@ -1,46 +1,58 @@
 function collapseDefinitions() {
     function toggleVisibility() {
-        const dds = document.querySelectorAll('dl dd');
+        const dds = document.querySelectorAll('#content dl dd');
         dds.forEach(dd => {
             if (dd.classList.contains('hidden')) {
-                console.log('hidden');
                 dd.classList.remove('hidden');
                 dd.classList.add('visible');
+                document.querySelectorAll('.collapse-all-defs-button').forEach(button => {
+                    button.innerHTML = '▲';
+                    button.title = 'Collapse all definitions';
+                });
             } else {
-                console.log('visible');
                 dd.classList.add('hidden');
                 dd.classList.remove('visible');
+                document.querySelectorAll('.collapse-all-defs-button').forEach(button => {
+                    button.innerHTML = '▼';
+                    button.title = 'Expand all definitions';
+                });
             }
         });
     }
     // Add button as last child of every <dl>
-    document.querySelectorAll('dt').forEach(dt => {
+    document.querySelectorAll('dt > span > span > span').forEach(dt => {
         const button = document.createElement('button');
-        button.classList.add('collapse-definitions-button')
-        button.innerHTML = 'Toggle';
+        button.classList.add('collapse-all-defs-button', 'btn');
+        button.innerHTML = '▲';
         button.setAttribute('id', 'toggleButton');
-        dt.appendChild(button);
+
+        const sup = document.createElement('sup');
+        sup.classList.add('collapse-all-defs-button-sup');
+        sup.appendChild(button);
+
+        dt.appendChild(sup);
     });
 
-    // Via event delegation add event listener to every .collapse-definitions-button element
+    // Via event delegation add event listener to every .collapse-all-defs-button element
     document.addEventListener('click', event => {
-        if (event.target.classList.contains('collapse-definitions-button')) {
+        if (event.target.classList.contains('collapse-all-defs-button')) {
             toggleVisibility();
 
-            const rect = event.target.getBoundingClientRect();
-            console.log('rect: ', rect);
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            console.log('scrollTop: ', scrollTop);
-            const elementTop = rect.top + scrollTop;
-            console.log('elementTop: ', elementTop);
-
-            window.scrollTo({
-                top: elementTop,
-                behavior: 'smooth'
+            event.target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'nearest'
             });
+
+            // Adjust the scroll position by 100px smoothly
+            setTimeout(() => {
+                window.scrollBy({
+                    top: -100,
+                    behavior: 'smooth'
+                });
+            }, 500); // Delay to ensure the initial scrollIntoView completes
         }
     });
-
 }
 
 document.addEventListener("DOMContentLoaded", function () {
