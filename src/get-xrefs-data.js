a23f1084e07c43522e00dff00f5a7788179d5b81
kor <kor@dwarshuis.com>
2024-09-07 17:23:12 +0200

    Add link to documentation website

--- src/get-xrefs-data.js
+++ src/get-xrefs-data.js
@@ -46,7 +46,7 @@ function getXrefsData(GITHUB_API_TOKEN) {
     function checkRateLimit(response) {
         if (response.status === 403 && response.headers.get('X-RateLimit-Remaining') === '0') {
             const resetTime = new Date(response.headers.get('X-RateLimit-Reset') * 1000);
-            console.error(`\n   SPEC-UP-T: Github API rate limit exceeded. Try again after ${resetTime}` + "\n");
+            console.error(`\n   SPEC-UP-T: Github API rate limit exceeded. Try again after ${resetTime}. See https://blockchainbird.github.io/spec-up-t-website/docs/github-token/ for more info.` + "\n");
             return true;
         } else {
             console.log(`\n   SPEC-UP-T: Github API rate limit: ${response.headers.get('X-RateLimit-Remaining')} requests remaining. See https://blockchainbird.github.io/spec-up-t-website/docs/github-token/ for more info.` + "\n");
