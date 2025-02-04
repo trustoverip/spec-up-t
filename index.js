f8dbf7fd3d89636f0190a63299bad4c90e969c77
Kor Dwarshuis <proximus@fastmail.com>
2025-02-04 12:13:45 +0100

    Remove confusing console.log

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ module.exports = async function (options = {}) {
   try {
     // Run the initialization first
     await initialize();
-    console.log('Initialization complete. Proceeding with the rest of the module logic...');
 
     const fs = require('fs-extra');
     const path = require('path');
