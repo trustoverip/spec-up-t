7cac3892567df1a385f7c0706cc33ff94bef7b84
Kor Dwarshuis <proximus@fastmail.com>
2025-02-06 13:32:24 +0100

    Remove unused require()'s

--- src/init.js
+++ src/init.js
@@ -1,12 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
-const { exec } = require('child_process');
 const outputDir = path.join(process.cwd(), 'output');
 const initFlagPath = path.join(outputDir, 'init.flag');
-let config = fs.readJsonSync(path.join(process.cwd(), 'specs.json'));
-
 const collectExternalReferences = require('./collectExternalReferences.js').collectExternalReferences;
-const { prepareTref } = require('./prepare-tref');
 
 async function initialize() {
     try {
