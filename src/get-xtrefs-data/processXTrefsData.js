01394460ace5cbc0668f67cabb77d7dcf9aee1c4
Kor Dwarshuis <proximus@fastmail.com>
2025-02-06 11:24:45 +0100

    Modify to make it work with a different return value

--- src/get-xtrefs-data/processXTrefsData.js
+++ src/get-xtrefs-data/processXTrefsData.js
@@ -7,26 +7,17 @@ async function processXTrefsData(allXTrefs, GITHUB_API_TOKEN, outputPathJSON, ou
     try {
         for (let xtref of allXTrefs.xtrefs) {
             // Go and look if the term is in the external repository and if so, get the commit hash, and other meta info plus the content of the file
-            const fetchedData = await fetchTermsFromGitHubRepository(GITHUB_API_TOKEN, xtref.term, xtref.owner, xtref.repo, xtref.terms_dir);
+            const item = await fetchTermsFromGitHubRepository(GITHUB_API_TOKEN, xtref.term, xtref.owner, xtref.repo, xtref.terms_dir);
 
-            // A lot of meta info is returned anyway but maybe there are no matches (items) 
-            if (fetchedData.data.items.length === 0) {
+            // // Check if fetchedData.data is defined
+            if (item !== null && matchTerm(item.content, xtref.term)) {
+                xtref.commitHash = item.sha;
+                xtref.content = item.content;
+                console.log(`\n   SPEC-UP-T: Match found for term: ${xtref.term} in ${xtref.externalSpec};`);
+            } else {
                 xtref.commitHash = "not found";
                 xtref.content = "This term was not found in the external repository.";
-            } else {
-                // In case there are matches (items) we need to check if the term is in the content of the file
-                fetchedData.data.items.forEach(item => {
-
-                    if (matchTerm(item.content, xtref.term)) {
-                        xtref.commitHash = item.sha;
-                        xtref.content = item.content;
-                        console.log(`\n   SPEC-UP-T: Match found for term: ${xtref.term} in ${xtref.externalSpec};`);
-                    } else {
-                        xtref.commitHash = "not found";
-                        xtref.content = "This term was not found in the external repository.";
-                        console.log(`\n   SPEC-UP-T: No match found for term: ${xtref.term} in ${xtref.externalSpec};`);
-                    }
-                });
+                console.log(`\n   SPEC-UP-T: No match found for term: ${xtref.term} in ${xtref.externalSpec};`);
             }
         }
 
@@ -38,7 +29,7 @@ async function processXTrefsData(allXTrefs, GITHUB_API_TOKEN, outputPathJSON, ou
 
         require('../../index.js')({ nowatch: true });
     } catch (error) {
-        console.error('An error occurred:', error);
+        console.error("An error occurred:", error);
     }
 }
 
