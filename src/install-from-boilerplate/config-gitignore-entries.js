e96043e7387a6edabef6ced2ca11fbdd8e301e2e
Kor Dwarshuis <proximus@fastmail.com>
2025-02-12 17:15:24 +0100

    Update .gitignore file

---
+++ src/install-from-boilerplate/config-gitignore-entries.js
@@ -0,0 +1,18 @@
+// Configuration
+const gitIgnoreEntries = {
+    gitignorePath: path.join(__dirname, '../../../../', '.gitignore'),
+    filesToAdd: ['node_modules',
+        '*.log',
+        'dist',
+        '*.bak',
+        '*.tmp',
+        '.DS_Store',
+        '.env',
+        'coverage',
+        'build',
+        '.history',
+        'speciaalgeval'
+],
+};
+
+module.exports = { gitIgnoreEntries };
--- src/install-from-boilerplate/custom-update.js
+++ src/install-from-boilerplate/custom-update.js
@@ -1,9 +1,11 @@
 const { configScriptsKeys } = require('./config-scripts-keys');
 const addScriptsKeys = require('./add-scripts-keys');
 const copySystemFiles = require('./copy-system-files');
+const { gitIgnoreEntries } = require('./config-gitignore-entries');
+const {updateGitignore} = require('./add-gitignore-entries');
 
 addScriptsKeys(configScriptsKeys);
-
 copySystemFiles();
+updateGitignore(gitIgnoreEntries.gitignorePath, gitIgnoreEntries.filesToAdd);
 
 console.log("✅ custom update done");
