cb2a3b872d5ae4f9ab1ec57b33b05377871cf506
Kor Dwarshuis <proximus@fastmail.com>
2025-02-15 09:52:36 +0100

    Fix wrong URL to info about Personal Access Tokens

--- src/collectExternalReferences/checkRateLimit.js
+++ src/collectExternalReferences/checkRateLimit.js
@@ -5,10 +5,10 @@ function checkRateLimit(response) {
 
     if (response.status === 403 && remaining === '0') {
         const resetTime = new Date(reset * 1000);
-        console.error(`\n   SPEC-UP-T: Github API rate limit exceeded. Try again after ${resetTime}. See https://trustoverip.github.io/spec-up-t-website/docs/github-token/ for more info.\n`);
+        console.error(`\n   SPEC-UP-T: Github API rate limit exceeded. Try again after ${resetTime}. See https://trustoverip.github.io/spec-up-t-website/docs/getting-started/github-token for more info.\n`);
         return true;
     } else if (remaining !== null) {
-        console.log(`\n   SPEC-UP-T: Github API rate limit: ${remaining} requests remaining. See https://trustoverip.github.io/spec-up-t-website/docs/github-token/ for more info.\n`);
+        console.log(`\n   SPEC-UP-T: Github API rate limit: ${remaining} requests remaining. See https://trustoverip.github.io/spec-up-t-website/docs/getting-started/github-token for more info.\n`);
     } else {
         console.warn(`\n   SPEC-UP-T: Unable to determine rate limit status. Check your GitHub API token and network connection.\n`);
     }
