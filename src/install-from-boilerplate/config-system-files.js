e6c5a728486b3475d8c1887667156a0b89998dc5
Kor Dwarshuis <proximus@fastmail.com>
2025-04-03 13:37:30 +0200

    Add additional workflow files to system files configuration

--- src/install-from-boilerplate/config-system-files.js
+++ src/install-from-boilerplate/config-system-files.js
@@ -1,3 +1,3 @@
-const systemFiles = ['README.md','.env.example','.github/workflows/menu.yml'];
+const systemFiles = ['README.md', '.env.example', '.github/workflows/menu.yml', '.github/workflows/render-specs.yml', '.github/workflows/set-gh-pages.yml'];
 
 module.exports = { systemFiles };
