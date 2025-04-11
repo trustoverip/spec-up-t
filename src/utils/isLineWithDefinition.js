0787ef4dee3cf5da0675707427ca52a68cddad4a
Kor Dwarshuis <proximus@fastmail.com>
2025-04-11 21:25:28 +0200

    Refactor isLineWithDefinition function to remove async keyword and clarify comment

--- src/utils/isLineWithDefinition.js
+++ src/utils/isLineWithDefinition.js
@@ -1,8 +1,7 @@
-async function isLineWithDefinition(line) {
+function isLineWithDefinition(line) {
     if (!line || typeof line !== 'string') return false;
 
     // Check if the line starts with [[def: and contains ]]
-    // instead of checking if it ends with ]]
     return line.startsWith('[[def:') && line.includes(']]');
 }
 
