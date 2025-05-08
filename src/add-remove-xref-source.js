47c1aa6ca8d01cae3a37bbe1096c03bfa67ddfee
Kor Dwarshuis <proximus@fastmail.com>
2025-05-08 16:46:36 +0200

    Remove commented-out console log in updateJSON function

--- src/add-remove-xref-source.js
+++ src/add-remove-xref-source.js
@@ -125,7 +125,6 @@ function updateJSON() {
         externalSpecs.push(inputs);
 
         fs.writeFileSync(JSON_FILE, JSON.stringify(data, null, 2), 'utf8');
-        // console.log(`Updated ${JSON_FILE} successfully.`);
         console.log(`Updated successfully.`);
     } catch (error) {
         console.error(`Error: Failed to update ${JSON_FILE}.`, error.message);
