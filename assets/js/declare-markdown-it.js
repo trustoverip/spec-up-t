2a1f0adf1596da067f432e192af8c5810c7deb23
Kor Dwarshuis <proximus@fastmail.com>
2025-05-08 21:08:30 +0200

    Refactor: Change variable declaration from var to const for markdownit instance

--- assets/js/declare-markdown-it.js
+++ assets/js/declare-markdown-it.js
@@ -1,5 +1,5 @@
 
-var md = window.markdownit({
+const md = window.markdownit({
    html: true, // Allow raw HTML in Markdown
    linkify: true, // Automatically convert URLs to clickable links
    typographer: true // Enable smart quotes and other typographic enhancements
