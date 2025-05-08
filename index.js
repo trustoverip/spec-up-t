3703ba1095e567b1c02d9912fda0825a933003a0
Kor Dwarshuis <proximus@fastmail.com>
2025-05-08 10:28:30 +0200

    Remove prepareTref module and its references to simplify codebase

--- index.js
+++ index.js
@@ -42,8 +42,6 @@ module.exports = async function (options = {}) {
 
     const { fixMarkdownFiles } = require('./src/fix-markdown-files.js');
 
-    // const { prepareTref } = require('./src/prepare-tref.js');
-
     let template = fs.readFileSync(path.join(modulePath, 'templates/template.html'), 'utf8');
     let assets = fs.readJsonSync(modulePath + '/src/asset-map.json');
     let externalReferences;
